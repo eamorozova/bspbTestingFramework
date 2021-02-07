bba12ef513fc6a23809009dbb9ad50ee922e369a
Ekaterina <ekaterina.a.morozova@gmail.com>
2021-02-08 00:02:17 +0300

    init project

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    node: true,
+    es6: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'plugin:jest/recommended'],
+  parser: 'babel-eslint',
+  parserOptions: {
+    ecmaVersion: 2018,
+    parser: 'babel-eslint',
+    ecmaFeatures: {
+      experimentalObjectRestSpread: true,
+    },
+  },
+  plugins: ['jest'],
+
+  rules: {
+    'import/prefer-default-export': 'off',
+    'linebreak-style': 0,
+    allowForLoopAfterthoughts: 0,
+  },
+  settings: {
+    jest: {
+      version: 26,
+    },
+  },
+};
